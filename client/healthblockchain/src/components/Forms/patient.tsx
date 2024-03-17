bbd2d09e06713d27752f6d9014f3c9626fba4320
RonexLemon <ronexondimu@gmail.com>
2024-03-17 10:12:09 +0300

    rector form

--- client/healthblockchain/src/components/Forms/patient.tsx
+++ client/healthblockchain/src/components/Forms/patient.tsx
@@ -3,11 +3,11 @@ import React from "react";
 
 const PatientForm =()=>{
     return(
-        <div className="flex justify-center items-center h-screen">
-<div className="h-full w-full">
+        <div className="flex justify-center items-center ">
+<div className="h-full w-full mt-10">
     <form>
-        <label>Patient Particulars</label>
-        <div className="w-full h-1/4">
+        <label className="text-5xl">Patient Particulars</label>
+        <div className="w-full  mb-5">
             <div className="flex ">
                 <div className="flex items-center justify-around">
                     <label>Surname</label>
@@ -26,8 +26,8 @@ const PatientForm =()=>{
 
                 </div>
                 <div className="flex items-center justify-around">
-                    <label>Membership No:</label>
-                    <input type="text" name="name" className="border-b-2 border-black bg-transparent outline-none text-center "/>
+                    <label className="text-sm">Membership No:</label>
+                    <input type="text" name="name" className="border-b-2 border-black bg-transparent outline-none text-center w-1/4 "/>
 
                 </div>
                
@@ -56,13 +56,13 @@ const PatientForm =()=>{
             {/* //third */}
             <div className="flex ">
                 <div className="flex items-center justify-around">
-                    <label>Name of the Member <span className="text-sm">if a patient is dependable</span></label>
-                    <input type="text" name="name" className="border-b-2 border-black bg-transparent outline-none text-center "/>
+                    <label className="text-sm">Name of the Member <span className="text-sm">if a patient is dependable</span></label>
+                    <input type="text" name="name" className="border-b-2 border-black bg-transparent outline-none text-center w-1/2 "/>
 
                 </div>
                 <div className="flex items-center justify-around">
-                    <label>Patient Relationship with member/employee</label>
-                    <input type="text" name="name" className="border-b-2 border-black bg-transparent outline-none text-center "/>
+                    <label className="text-sm">Patient Relationship with member/employee</label>
+                    <input type="text" name="name" className="border-b-2 border-black bg-transparent outline-none text-center w-1/2 "/>
 
                 </div>
                
@@ -86,33 +86,33 @@ const PatientForm =()=>{
 
         </div>
 
-        <div>
-            <span>Consultation/Referrals to specialist and hospital admission</span>
+        <div className="w-full mb-4">
+            <span className="text-2xl" >Consultation/Referrals to specialist and hospital admission</span>
         </div>
-        <label className="text-black font-bold">Details to be filed by attending doctor.</label>
+        <label className="text-black font-bold ">Details to be filed by attending doctor.</label>
 
-        <div className="w-3/4 h-1/4 flex flex-col">
+        <div className="w-3/4 h-1/4 flex flex-col mb-4">
         <div className="flex items-start justify-between ">
                     <label>Nature of sickness/Aliment</label>
-                    <input type="text" name="name" className="border  border-b-2"/>
+                    <input type="text" name="name" className="border-b-2 border-black bg-transparent outline-none text-center "/>
 
                 </div>
-                <div className="flex items-center justify-around">
+                <div className="flex  items-start justify-between ">
                     <label>When it first occured</label>
-                    <input type="text" name="name" className="border  border-b-2"/>
+                    <input type="text" name="name" className="border-b-2 border-black bg-transparent outline-none text-center "/>
 
                 </div>
 
-                <div className="flex items-center justify-around">
+                <div className="flex  items-start justify-between ">
                     <label>Nature of treatment</label>
-                    <input type="text" name="name" className="border  border-b-2"/>
+                    <input type="text" name="name" className="border-b-2 border-black bg-transparent outline-none text-center "/>
 
                 </div>
 
         </div>
 
-        <div>
-            <span>Details of Medical expense</span>
+        <div className="w-full">
+            <span className="text-2xl">Details of Medical Expense</span>
         </div>
         <div className="flex flex-col h-1/4 w-full">
             <table className="w-full">
