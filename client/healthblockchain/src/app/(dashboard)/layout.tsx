d13080a055ca19ebe7846849b80416fb688ee46d
RonexLemon <ronexondimu@gmail.com>
2024-03-17 12:03:28 +0300

    fix:layout

--- client/healthblockchain/src/app/(dashboard)/layout.tsx
+++ client/healthblockchain/src/app/(dashboard)/layout.tsx
@@ -6,12 +6,12 @@ export default function DashboardLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <main className="min-h-screen bg-[#002062] overflow-hidden">
+    <main className="min-h-screen    bg-[#002062] overflow-hidden">
       <div className="grid grid-cols-4 w-full">
-        <div className="col-span-1/2 h-screen overflow-y-auto">
+        <div className="col-span-1 h-screen overflow-y-auto">
           <SideBar />
         </div>
-        <div className="col-span-3 bg-[#002062] h-screen overflow-y-auto">
+        <div className="col-span-3 p-0 bg-[#002062] h-screen w-full   overflow-y-auto">
           {children}
         </div>
       </div>
--- client/healthblockchain/src/components/dashboard/patienceDashboard.tsx
+++ client/healthblockchain/src/components/dashboard/patienceDashboard.tsx
@@ -38,6 +38,12 @@ export default function PatienceDashboard(){
 
     return(
         <div className="w-full h-screen mt-5">
+            <div className="grid grid-cols-6 ">
+                <div className="h-full col-span-4">
+
+                
+
+            
             <div className="flex justify-between items-center" >
                 <div className="mb-1">
                 <span className="text-white">Dashboard Overview</span>
@@ -45,7 +51,7 @@ export default function PatienceDashboard(){
                     </div>
                     
                    
-                    <div className=" flex justify-between w-3/4  h-1/4 rounded-xl bg-gradient-to-r from-indigo-500 from-10% to-emerald-500 to-90%">
+                    <div className=" flex justify-between w-full   rounded-xl bg-gradient-to-r from-indigo-500 from-10% to-emerald-500 to-90%">
                         <div className="flex flex-col  p-10 text-white">
                             <span className="font-bold">{session?.user?.user?.firstName}  {session?.user?.user?.lastName},</span>
                             <span className="text-sm">Have a nice day and don`t forget to take<br/> care of your health</span>
@@ -55,19 +61,19 @@ export default function PatienceDashboard(){
                         <Image src={dash}  alt=""/>
 
                     </div>
-                    <div className="flex w-3/4 mt-8 items-center justify-center">
+                    <div className="flex w-full mt-8 items-center justify-center">
   <button className="bg-[#181d46] hover:bg-[#1B2269] text-white font-bold py-2 px-4 rounded w-1/2 ">
     Claim Status
   </button>
 </div>
 
 
-                    <div className="flex justify-between items-center w-3/4 h-1/4  ">
+                    <div className="flex justify-between items-center w-full h-1/4  ">
                        <div className="flex  items-center justify-around bg-blend-lighten bg-blue-900   shadow h-1/2 w-1/4 rounded-xl p-5">
                             <Image src={dash} alt="" className="h-3/4 w-1/4"/>
                             <div className="flex flex-col justify-center text-white">
-                                <span className="text-sm text-gray-400 flex  "> <h3 className=" font-bold text-white ">{202}/</h3>3000</span>
-                                <span>Step taken</span>
+                                <span className="text-sm text-gray-400 flex  "> Claim</span>
+                                <span className="text-gray-400">Approved</span>
                             </div>
                             
 
@@ -75,8 +81,8 @@ export default function PatienceDashboard(){
                        <div className="flex  items-center justify-around bg-blend-lighten bg-blue-900   shadow h-1/2 w-1/4 rounded-xl p-5">
                             <Image src={dash} alt="" className="h-3/4 w-1/4"/>
                             <div className="flex flex-col justify-center text-white">
-                                <span className="text-sm text-gray-400 flex gap-2 "> <h3 className=" font-bold text-white ">{408}</h3>kcal</span>
-                                <span>Calories burned</span>
+                                <span className="text-sm text-gray-400 flex gap-2 ">Claim</span>
+                                <span className="text-gray-400">Pending</span>
                             </div>
                             
 
@@ -84,8 +90,8 @@ export default function PatienceDashboard(){
                         <div className="flex  items-center justify-around bg-blend-lighten bg-blue-900   shadow h-1/2 w-1/4 rounded-xl p-5">
                             <Image src={dash} alt="" className="h-3/4 w-1/4"/>
                             <div className="flex flex-col justify-center text-white">
-                                <span className="text-sm text-gray-400 flex gap-2 "> <h3 className=" font-bold text-white ">{87}</h3>litres</span>
-                                <span>Water Taken</span>
+                                <span className="text-sm text-gray-400 flex gap-2 ">Claim</span>
+                                <span className="text-gray-400">Rejected</span>
                             </div>
                             
 
@@ -95,14 +101,14 @@ export default function PatienceDashboard(){
                     </div>
                    {/* //claim details */}
 
-                   <div className="flex w-3/4 mt-8 items-center justify-center">
+                   <div className="flex w-full mt-8 items-center justify-center mb-5">
   <button className="bg-[#181d46] hover:bg-[#1B2269] text-white font-bold py-2 px-4 rounded w-1/2 ">
     Claim Details
   </button>
 </div>
 
                     {/* reminders */}
-                      <div className="flex justify-between items-center w-3/4 h-1/4 mt-2  ">
+                      <div className="flex justify-between items-center w-full h-1/4 mt-2  ">
                        <div className="flex  items-center justify-around bg-blend-lighten bg-blue-900  h-full  shadow  w-1/4 rounded-xl p-5">
                             <Image src={dash} alt="" className="h-3/4 w-1/4"/>
                             <div className="flex flex-col justify-center text-white">
@@ -135,8 +141,19 @@ export default function PatienceDashboard(){
                     </div>
 
 
+                    </div>
+                    {/* //next column */}
+                    {/* <div className="flex flex-col w-full h-full items-center mt-6">
 
+                        <div className="flex items-center   bg-blend-lighten bg-blue-900   shadow h-1/4 w-3/4 rounded-xl ">
+                            <div className="flex items-center ">
+                            <Image src={dash} alt="" className="h-3/4 w-3/4"/>
+                            </div>
 
+                        </div>
+
+                    </div> */}
+                    </div>
         </div>
     )
 }
