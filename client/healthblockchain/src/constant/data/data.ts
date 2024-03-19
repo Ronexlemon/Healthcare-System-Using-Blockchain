19ecbf9678a7359172a760e7ebdf9735b3275ca3
RonexLemon <ronexondimu@gmail.com>
2024-03-19 15:22:01 +0300

    static:data

---
+++ client/healthblockchain/src/constant/data/data.ts
@@ -0,0 +1,9 @@
+export const data =[{"name":"johnDoe","status":"waiting","charges":10000,"patientId":23456,"hospitalID":394994},
+{"name":"johnDoe","status":"waiting","charges":10000,"patientId":23456,"hospitalID":394994},
+{"name":"johnDoe","status":"waiting","charges":10000,"patientId":23456,"hospitalID":394994},
+{"name":"johnDoe","status":"waiting","charges":10000,"patientId":23456,"hospitalID":394994},
+{"name":"johnDoe","status":"waiting","charges":10000,"patientId":23456,"hospitalID":394994},
+{"name":"johnDoe","status":"waiting","charges":10000,"patientId":23456,"hospitalID":394994},
+{"name":"johnDoe","status":"waiting","charges":10000,"patientId":23456,"hospitalID":394994},
+{"name":"johnDoe","status":"waiting","charges":10000,"patientId":23456,"hospitalID":394994},
+{"name":"johnDoe","status":"waiting","charges":10000,"patientId":23456,"hospitalID":394994}]
