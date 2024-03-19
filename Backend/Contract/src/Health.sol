e2b800264608f55d049d6996e4015b61195612b0
RonexLemon <ronexondimu@gmail.com>
2024-03-19 17:33:04 +0300

    remove:openzepplin file

--- Backend/Contract/src/Health.sol
+++ Backend/Contract/src/Health.sol
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.2;
-//import "@openzeppelin/contracts/utils/Counters.sol";
+import "../node_modules/@openzeppelin/contracts/utils/Counters.sol";
 
-import "openzeppelin-contracts/utils/Counters.sol";
+//import "openzeppelin-contracts/contracts/utils/Counters.sol";
 
 
 
